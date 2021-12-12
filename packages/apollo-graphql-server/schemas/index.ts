86d89604c4e8700486439da9aec2d43db8ed98f7
Errong, Leng <errong.leng@gmail.com>
2021-12-12 02:44:55 -0800

    remove duplicated schema.graphql file
    *.graphql files are placed at /public folder as static resources
    otherwise files are not accessible if this was called
    in a serverless lambda function(such as /api/graphql endpoint)

--- packages/apollo-graphql-server/schemas/index.ts
+++ packages/apollo-graphql-server/schemas/index.ts
@@ -5,6 +5,9 @@ import { resolvers } from '../resolvers'
 import path from 'path'
 
 export const loadSchemas = () => {
+  // *.graphql files are placed at /public folder as static resources
+  // otherwise files are not accessible if this was called
+  // in a serverless lambda function(such as /api/graphql endpoint)
   const dir = path.resolve('./public', 'graphql')
   const schemaPath = path.join(dir, 'schemas/**/*.graphql')
   const schema = loadSchemaSync(schemaPath, {
