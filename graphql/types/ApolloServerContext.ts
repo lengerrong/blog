716b5fd38d99bfe46db10d0fb63b3739b9a2d000
Errong, Leng <errong.leng@gmail.com>
2021-12-20 22:06:02 -0800

    remove unused files

--- graphql/types/ApolloServerContext.ts
+++
@@ -1,9 +0,0 @@
-import { MicroRequest } from 'apollo-server-micro/dist/types'
-import { ServerResponse } from 'http'
-
-type ApolloServerContext = {
-  req: MicroRequest
-  res: ServerResponse
-}
-
-export default ApolloServerContext
