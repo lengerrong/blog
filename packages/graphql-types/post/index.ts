c263e32340c0b9546970ec6962cd5e26b0cfcbce
Errong, Leng <errong.leng@gmail.com>
2022-03-10 21:51:14 -0800

    initial infinite posts loading
    TODO:
    graphql resolvers

---
+++ packages/graphql-types/post/index.ts
@@ -0,0 +1,17 @@
+export type Post = {
+  slug: string
+  canonical_url?: string
+  codeinjection_head?: string
+  codeinjection_foot?: string
+  featured: number
+  feature_image?: string
+  title: string
+  author_id: string
+  plaintext: string
+  html: string
+  mobiledoc?: string
+  comment_id: string
+  published_at: Date
+  created_at: Date
+  updated_at: Date
+}
---
+++ pages/index.tsx
@@ -0,0 +1,73 @@
+import { useInfiniteQuery } from 'react-query'
+import { Query, Post } from 'graphql-types'
+import { SyntheticEvent } from 'react'
+
+const POST_DEFAULT_OFFSET = 0
+const SCROLL_TO_NEXT_PAGE_THRESHOLD_RATE = 1.5
+
+const App = () => {
+  type FetchPostsOptions = {
+    offset: number
+  }
+  const fetchPosts = async ({
+    offset
+  }: FetchPostsOptions): Promise<Query<Post>> => {
+    const response = await fetch('/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({ query: `{posts(${offset})}` })
+    })
+    return response.json() as Promise<Query<Post>>
+  }
+
+  const { data, hasNextPage, fetchNextPage, isFetching } = useInfiniteQuery(
+    'posts',
+    ({ pageParam }) => fetchPosts(pageParam as FetchPostsOptions),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (!lastPage.hasMore) return undefined
+        return {
+          offset: pages.reduce(
+            (offset, page) => (offset += page.count),
+            POST_DEFAULT_OFFSET
+          )
+        }
+      }
+    }
+  )
+
+  const onScroll = (e: SyntheticEvent) => {
+    ;(async () => {
+      const element = e.target as HTMLDivElement
+      const { scrollHeight, scrollTop, clientHeight } = element
+      if (
+        !isFetching &&
+        hasNextPage &&
+        scrollHeight - scrollTop <=
+          clientHeight * SCROLL_TO_NEXT_PAGE_THRESHOLD_RATE
+      ) {
+        await fetchNextPage()
+      }
+    })().catch(() => undefined)
+  }
+
+  return (
+    <div onScroll={onScroll}>
+      {data?.pages.map((page) =>
+        page.items
+          .map((item) => item.value)
+          .map((post) => (
+            <>
+              {post.title}
+              {post.plaintext}
+            </>
+          ))
+      )}
+    </div>
+  )
+}
+
+export default App
