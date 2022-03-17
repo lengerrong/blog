5041c02447867d6ef554ef34bffc65f606788af0
Errong, Leng <errong.leng@gmail.com>
2022-03-16 22:52:37 -0700

    temprary disable tests for oracle db
    oracle always free db stopped after no activities in 7 days

--- packages/oracledb/tests/index.test.ts
+++ packages/oracledb/tests/index.test.ts
@@ -12,7 +12,7 @@ describe('OracleSODARestApi', () => {
       ),
       'https://gb155dd5f199f4a-errongwindev.adb.us-sanjose-1.oraclecloudapps.com/ords/'
     )
-    const tests = await api.collection('tests')
-    expect(tests?.data?.items?.[0]?.value?.hello).toEqual('world')
+    //const tests = await api.collection('tests')
+    //expect(tests?.data?.items?.[0]?.value?.hello).toEqual('world')
   })
 })
