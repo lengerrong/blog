e81bf9b3ef696ebf394b3d8b71315f97c80a30b4
Errong, Leng <errong.leng@gmail.com>
2022-03-17 03:01:46 -0700

    implement posts query

--- packages/apollo-graphql-server/resolvers/index.ts
+++ packages/apollo-graphql-server/resolvers/index.ts
@@ -1,30 +1,7 @@
-import { AuthenticationError } from 'apollo-server-micro'
-import { ApolloServerContext } from '../types/ApolloServerContext'
-
-const fakeDatabase = {
-  id: ''
-}
-
-type User = {
-  id: string
-}
+import posts from './posts'
 
 export const resolvers = {
   Query: {
-    getUser: () => {
-      return {
-        id: fakeDatabase.id
-      }
-    }
-  },
-  Mutation: {
-    setUser: (_: any, user: User, context: ApolloServerContext) => {
-      const { req } = context
-      if (!req.headers.authorization) {
-        throw new AuthenticationError('you must be logged in')
-      }
-      fakeDatabase.id = user.id
-      return fakeDatabase.id
-    }
+    posts
   }
 }
---
+++ packages/apollo-graphql-server/resolvers/posts/index.ts
@@ -0,0 +1,53 @@
+import { GraphQLSchemaContext } from 'apollo-server-types'
+import { GraphQLResolveInfo } from 'graphql'
+import { Post, Posts, PostsParams } from 'graphql-types'
+import OracleDB from 'oracledb'
+import log from '../../../../utils/logging'
+
+const posts = async (
+  source: any,
+  { offset }: PostsParams,
+  context: GraphQLSchemaContext,
+  info: GraphQLResolveInfo
+) => {
+  log.info(source, offset, context, info)
+  const db = new OracleDB(
+    {
+      client_id: process.env.ORACLE_DB_OAUTH_CLIENT_ID!,
+      client_secret: process.env.ORACLE_DB_OAUTH_CLIENT_SECRET!
+    },
+    process.env.ORACLE_DB_ORDS_URL!,
+    process.env.ORACLE_DB_SCHEMA
+  )
+  const limit = 49
+  const offsetPosts = await db.collection<Post>(
+    'POSTS',
+    {
+      action: 'query',
+      fields: 'all',
+      limit,
+      offset: offset,
+      totalResults: true
+    },
+    {
+      $query: {
+        type: 'post',
+        status: 'published'
+      },
+      $orderby: [
+        {
+          path: 'published_at',
+          order: 'desc'
+        }
+      ]
+    }
+  )
+  const { hasMore, count } = offsetPosts
+  return {
+    items: offsetPosts.items.map((item) => item.value),
+    hasMore,
+    count
+  } as Posts
+}
+
+export default posts
