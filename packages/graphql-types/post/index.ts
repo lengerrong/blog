127fce217cc8a46e9acea6c53d3712f6031fe5ce
Errong, Leng <errong.leng@gmail.com>
2022-03-17 02:46:57 -0700

    update Post types

--- packages/graphql-types/post/index.ts
+++ packages/graphql-types/post/index.ts
@@ -1,17 +1,27 @@
 export type Post = {
   slug: string
-  canonical_url?: string
-  codeinjection_head?: string
-  codeinjection_foot?: string
+  canonical_url: string
+  codeinjection_head: string
+  codeinjection_foot: string
   featured: number
-  feature_image?: string
+  feature_image: string
   title: string
   author_id: string
   plaintext: string
   html: string
-  mobiledoc?: string
+  mobiledoc: string
   comment_id: string
   published_at: Date
   created_at: Date
   updated_at: Date
+}
+
+export type Posts = {
+  hasMore: boolean
+  items: [Post]
+  count: number
+}
+
+export type PostsParams = {
+  offset: number
 }
