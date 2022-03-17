a0cccfb958fa6cdd7b8a8d4038ec0240e82ee199
Errong, Leng <errong.leng@gmail.com>
2022-03-16 22:56:25 -0700

    use react query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,17 @@ import type { AppProps } from 'next/app'
 import MaterialUIProvider from 'ui/MaterialUIProvider'
 import i18n from 'ui/i18n'
 import { I18nextProvider } from 'react-i18next'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <I18nextProvider i18n={i18n}>
       <MaterialUIProvider>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </MaterialUIProvider>
     </I18nextProvider>
   )
