517bdac53984a5bcac87df91994f685cfa3e6ca2
Errong, Leng <errong.leng@gmail.com>
2022-03-19 04:03:02 -0700

    add dynamic routes for posts

---
+++ pages/[slug].tsx
@@ -0,0 +1,89 @@
+import { Post, PostData, SlugsData } from 'apollo-graphql-types'
+
+export type SlugPostProps = {
+  post: Post
+}
+
+const SlugPost = ({ post }: SlugPostProps) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: post?.html
+      }}
+    ></div>
+  )
+}
+
+export default SlugPost
+
+const fetchSlugs = async () => {
+  const response = await fetch(
+    new URL('/api/graphql', process.env.BLOG_URL).toString(),
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query: `{
+        slugs
+      }`
+      })
+    }
+  )
+  const slugsData = (await response.json()) as SlugsData
+  return slugsData.data.slugs
+}
+
+export type SlugProps = {
+  slug: string
+}
+
+export type SlugParamsProps = {
+  params: SlugProps
+}
+
+export async function getStaticPaths() {
+  const slugs = await fetchSlugs()
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug
+      }
+    })),
+    fallback: true
+  }
+}
+
+const fetchPost = async (slug: string) => {
+  const response = await fetch(
+    new URL('/api/graphql', process.env.BLOG_URL).toString(),
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query: `{
+        post(slug: "${slug}") {
+          html
+        }
+      }`
+      })
+    }
+  )
+  const postData = (await response.json()) as PostData
+  return postData.data.post
+}
+
+export async function getStaticProps({ params }: SlugParamsProps) {
+  const { slug } = params
+  const post = await fetchPost(slug)
+  return {
+    props: {
+      post
+    }
+  }
+}
