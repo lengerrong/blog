6bffac5a6c9bcbb60c8c8dd74af89b2e88b9f60a
Errong, Leng <errong.leng@gmail.com>
2022-03-19 04:55:31 -0700

    fix graphql api url

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,6 @@ import log from 'logging'
 export const graphqlAPIURL = () => {
   const path = '/api/graphql'
   if (typeof window !== 'undefined' && typeof window.document !== 'undefined') {
-    log.info('graphql api', path)
     return path
   }
   if (
@@ -11,8 +10,7 @@ export const graphqlAPIURL = () => {
     process?.env?.VERCEL_ENV === 'development'
   ) {
     log.info('graphql api', `https:// + ${process.env.VERCEL_URL!} + ${path}`)
-    return `https:// + ${process.env.VERCEL_URL!} + ${path}`
+    return `https://${process.env.VERCEL_URL!}${path}`
   }
-  log.info('graphql api', `${process.env.BLOG_URL!}${path}`)
   return `${process.env.BLOG_URL!}${path}`
 }
