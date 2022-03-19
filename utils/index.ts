7944139fb7588d03f505a6f26d39da86d390875e
Errong, Leng <errong.leng@gmail.com>
2022-03-19 05:03:22 -0700

    for all server side fetch, all goes to
    BLOG_URL/api/graphql
    BLOG_URL will be set in all env
    https://blog.errong.win

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,7 @@
-import log from 'logging'
-
 export const graphqlAPIURL = () => {
   const path = '/api/graphql'
   if (typeof window !== 'undefined' && typeof window.document !== 'undefined') {
     return path
-  }
-  if (
-    process?.env?.VERCEL_ENV === 'preview' ||
-    process?.env?.VERCEL_ENV === 'development'
-  ) {
-    log.info('graphql api', `https:// + ${process.env.VERCEL_URL!} + ${path}`)
-    return `https://${process.env.VERCEL_URL!}${path}`
   }
   return `${process.env.BLOG_URL!}${path}`
 }
