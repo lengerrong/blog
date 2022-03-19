974274bf153d1a74f432e59690708a6dbbc130b4
Errong, Leng <errong.leng@gmail.com>
2022-03-19 04:51:41 -0700

    log api graphql url

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,18 @@
+import log from 'logging'
+
 export const graphqlAPIURL = () => {
   const path = '/api/graphql'
+  if (typeof window !== 'undefined' && typeof window.document !== 'undefined') {
+    log.info('graphql api', path)
+    return path
+  }
   if (
     process?.env?.VERCEL_ENV === 'preview' ||
     process?.env?.VERCEL_ENV === 'development'
   ) {
+    log.info('graphql api', `https:// + ${process.env.VERCEL_URL!} + ${path}`)
     return `https:// + ${process.env.VERCEL_URL!} + ${path}`
   }
-  if (typeof window !== 'undefined' && typeof window.document !== 'undefined') {
-    return path
-  }
+  log.info('graphql api', `${process.env.BLOG_URL!}${path}`)
   return `${process.env.BLOG_URL!}${path}`
 }
